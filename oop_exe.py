7e75fac4b7fefa4aab96cd7d93eeb220608049fe
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-12 23:16:43 +0300

    more extra added

--- oop_exe.py
+++ oop_exe.py
@@ -179,7 +179,7 @@ class Customer:
     def __init__(self,name,balance):
         self.name=name
         self.balance=balance
-        self.points=0
+        self.points=50
     def purchase(self,item_name, price):
         if price<=self.balance:
             print(self.balance - price)
@@ -188,15 +188,156 @@ class Customer:
             print(f"Not enough balance for {item_name}.")
     def redeem(self):
         if self.points>=50:
-            print(self.balance+5)
+            self.balance+=5
             self.points=0
     def status(self):
         print(f"Name: {self.name} | Balance: ${self.balance} | Points: {self.points}")
-noa=Customer("noa",15.0)
+noa=Customer("noa",50)
 noa.purchase("bottle",20)
 noa.redeem()
 noa.status() 
+
+#3
+class Order:
+    def __init__(self,customer_name, items):
+        self.name=customer_name
+        self.items=items
+    def total_prep_time(self):
+        total=0
+        for item in self.items:
+            total+=item[1]
+        return total
+    def ready_by(self,minutes):
+        return self.total_prep_time()<=minutes
+    def print_order(self):
+        for item in self.items:
+            print(f"item: {item[0]} | time: {item[1]}")
+    def slowest_item(self):
+        print("slowest:",(max(self.items, key=lambda x: x[1])[0]))
+moshe=Order("moshe",[("latte",3),("sandwich",7),("smoothie",5)])
+moshe.total_prep_time()
+print(moshe.ready_by(10))
+print(moshe.ready_by(20))
+moshe.print_order()
+moshe.slowest_item()
+
+#4
+class coffeeShop:
+    def __init__(self,name):
+        self.name=name
+        self.revenue = 0.0
+    def sell(self,itam_name,price):
+        self.revenue+=price
+        print(f"item: {itam_name}price: ${price}")
+    def sell_discounted(self,item_name, price, discount):
+        self.revenue+=price-discount
+        print(f"item: {item_name}\nprice: {self.revenue}")
+    def daily_summary(self):
+        print(f"{self.name} | Daily revenue: ${self.revenue:.2f}") 
+cofee=coffeeShop("the bean")
+cofee.sell("coffee",15)
+cofee.sell_discounted("pasta",30,5)
+cofee.sell("coke",20)
+cofee.sell_discounted("soop",40,7)
+cofee.sell("egg",15)
+cofee.daily_summary()
+
+#5
+class Drink:
+    def __init__(self,name,bace_price,size):
+        self.name=name
+        self.bace_price=bace_price
+        self.siz=size
+    def final_price(self):
+        if self.siz == "small":
+            return self.bace_price
+        elif self.siz == "medium":
+            return self.bace_price*1.3
+        elif self.siz == "large":
+            return self.bace_price*1.6
+    def describe(self):
+        print(f"{self.name} ({self.siz}) ${self.final_price():.2f}")
+watter=Drink("watter",3.0,"small")
+watter.describe()
+watter=Drink("watter",3.90,"medium")
+watter.describe()
+watter=Drink("watter",4.80,"large")
+watter.describe()
+
+#6
+class Shift:
+    def __init__(self,barista_name, start_hour, end_hour, drinks_target):
+        self.name=barista_name
+        self.start=start_hour
+        self.end=end_hour
+        self.target=drinks_target
+    def duration(self):
+        return self.end-self.start
+    def drinks_per_hour(self):
+        return self.target//self.duration()
+    def is_long_shift(self):
+        return self.duration()>6
+    def describe(self):
+        print(f"barista: {self.name} | hours: {self.duration()} | target: {self.target} | per hour: {self.drinks_per_hour()}) | long shift: {self.is_long_shift()}")
+moish=Shift("moish",8,16,120)
+moish.describe()
+
+#7
+class MenuItem:
+    def __init__(self,name,price):
+        self.name=name
+        self.price=price
+    def HappyHour(self,start_hour, end_hour, discount_percent):
+        self.start=start_hour
+        self.end=end_hour
+        self.discount=discount_percent
+    def is_active(self,current_hour):
+        return True if self.start<=current_hour<self.end else False
+    def discounted_price(self):
+        return self.price*(1-self.discount/100)
+hour=MenuItem("beer",20.0)
+hour.HappyHour(16,18,20)
+print(hour.is_active(19))
+print(hour.discounted_price())
+
+#8
+class Combo:
+    def __init__(self,name,price):
+        self.name=name
+        self.price=price
+    def Combo(self, name, items, discount):
+        self.combo_name=name
+        self.items=items
+        self.discount=discount
+    def original_price(self):
+        return sum(self.items)
+    def combo_price(self):
+        return self.original_price()-self.discount
+    def savings(self):
+        return self.original_price()-self.combo_price() 
+    def describe(self):
+        print(f"{self.combo_name} | original: {self.original_price} | combo: {self.combo_price} | savings: {self.savings}")
+
+
+
         
+
+        
+
+
+
+        
+        
+
+
+
+        
+
+        
+
+
+
+               
 
 
         
